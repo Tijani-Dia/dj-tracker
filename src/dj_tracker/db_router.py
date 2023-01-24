8478961a0d7506aa071e2a8b32475530dec046ef
Tidiane Dia <atdia97@gmail.com>
2023-01-24 00:49:38 +0000

    Improve database router

--- src/dj_tracker/db_router.py
+++ src/dj_tracker/db_router.py
@@ -1,15 +1,26 @@
+from django.apps import apps
+
+from dj_tracker.utils import cached_attribute
+
+
 class DjTrackerRouter:
     app_label = "dj_tracker"
     db_alias = "trackings"
 
+    @cached_attribute
+    def models(cls):
+        return frozenset(
+            apps.get_app_config(cls.app_label).get_models(include_auto_created=True)
+        )
+
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == self.app_label:
+        if model in self.models:
             return self.db_alias
 
     db_for_write = db_for_read
 
-    def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if db == self.db_alias:
-            return app_label == self.app_label
+    def allow_migrate(self, db, app_label, **hints):
         if app_label == self.app_label:
+            return db == self.db_alias
+        if db == self.db_alias:
             return False
